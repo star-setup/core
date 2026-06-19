3bacb26eab2cdc19657b62f5efad54d693b6afd2
Masonlet <masonletoile@hotmail.com>
2026-06-19 01:57:38 -0400

    fix(test): remove double lock

--- src/main.rs
+++ src/main.rs
@@ -92,7 +92,7 @@ fn main() {
 
   if args.repo.is_none() {
     if std::io::stdin().is_terminal() {
-      if let Err(e) = interactive_mode(&mut args, &mut io::stdin().lock(), &mut io::stdout()) {
+      if let Err(e) = interactive_mode(&mut args, &mut stdin, &mut io::stdout()) {
         eprintln!("Error: {e}");
         std::process::exit(1);
       }
