5e4f02fdf99f3591c75a6aac192cdbb20e6bf5f1
Masonlet <masonletoile@hotmail.com>
2026-06-22 18:56:32 -0400

    fix: gate manifest embedding to Windows

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use embed_manifest::manifest::ExecutionLevel;
 use embed_manifest::{embed_manifest, new_manifest};
 
 fn main() {
-  if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+  if std::env::var_os("CARGO_CFG_WINDOWS").is_some() && cfg!(target_os = "windows") {
     embed_manifest(
       new_manifest("masonlet.star-setup").requested_execution_level(ExecutionLevel::AsInvoker),
     )
