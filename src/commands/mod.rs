8b91b9d6ad97fdeae86a245c406e2fb0a188e3c9
Masonlet <masonletoile@hotmail.com>
2026-06-24 14:30:57 -0400

    tests(commands): add tests for create_mono_repo_mesonbuild

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,6 +3,7 @@ pub mod header;
 pub mod mono;
 pub mod single;
 pub use mono::{
-  create_mono_repo_cmakelists, mono_repo_mode, resolve_repos_for_mono, resolve_test_repo,
+  create_mono_repo_cmakelists, create_mono_repo_mesonbuild, mono_repo_mode, resolve_repos_for_mono,
+  resolve_test_repo,
 };
 pub use single::single_repo_mode;
