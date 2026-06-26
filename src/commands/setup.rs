d4406919794555242b35b0f5e72bd3ce85f61c5e
Masonlet <masonletoile@hotmail.com>
2026-06-26 14:53:32 -0400

    fix: print clean during dry_run regardless of build dir existence

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -13,20 +13,20 @@ pub fn prepare_build_dir(
   clean: bool,
   ctx: &mut RunCtx<'_>,
 ) -> Result<(), String> {
-  if clean && build_path.exists() {
+  if clean && ctx.io.dry_run {
     writeln!(ctx.io.output, "Cleaning build directory\n").ok();
-    if ctx.io.dry_run {
-      writeln!(
-        ctx.io.output,
-        "Would run: fs::remove_dir_all({})",
-        build_path.display()
-      )
-      .ok();
-    } else {
-      crate::time!(ctx.io.timing, ctx.io.output, "Clean", {
-        fs::remove_dir_all(build_path).map_err(|e| e.to_string())?;
-      });
-    }
+    writeln!(
+      ctx.io.output,
+      "Would run: fs::remove_dir_all({})",
+      build_path.display()
+    )
+    .ok();
+  }
+  else if clean && build_path.exists() {
+    writeln!(ctx.io.output, "Cleaning build directory\n").ok();
+    crate::time!(ctx.io.timing, ctx.io.output, "Clean", {
+      fs::remove_dir_all(build_path).map_err(|e| e.to_string())?;
+    });
   }
 
   writeln!(ctx.io.output, "Creating build directory\n").ok();
