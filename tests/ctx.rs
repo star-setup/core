636285f7751e724bbddb1d94800d920ec077aabc
Masonlet <masonletoile@hotmail.com>
2026-06-26 00:02:21 -0400

    test(ctx): add ProcessRunner test using git --version

---
+++ tests/ctx.rs
@@ -0,0 +1,9 @@
+#[test]
+fn test_process_runner_runs_command() {
+  use star_setup::ctx::{IoCtx, ProcessRunner, Runner};
+  let mut input = b"".as_ref();
+  let mut output = Vec::new();
+  let mut runner = ProcessRunner;
+  let mut io = IoCtx { input: &mut input, output: &mut output, verbose: false, timing: false };
+  assert!(runner.run(&["git", "--version"], None, &mut io).is_ok());
+}
