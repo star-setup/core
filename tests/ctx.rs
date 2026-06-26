c29a264b8cfe6e6553fdb5fe1130ce9b45f2e600
Masonlet <masonletoile@hotmail.com>
2026-06-26 14:12:18 -0400

    test: add DryRunRunner tests

--- tests/ctx.rs
+++ tests/ctx.rs
@@ -1,6 +1,8 @@
+use star_setup::ctx::{DryRunRunner, IoCtx, ProcessRunner, Runner};
+use std::path::Path;
+
 #[test]
 fn test_process_runner_runs_command() {
-  use star_setup::ctx::{IoCtx, ProcessRunner, Runner};
   let mut input = b"".as_ref();
   let mut output = Vec::new();
   let mut runner = ProcessRunner;
@@ -12,4 +14,38 @@ fn test_process_runner_runs_command() {
     dry_run: false,
   };
   assert!(runner.run(&["git", "--version"], None, &mut io).is_ok());
+}
+
+#[test]
+fn test_dry_run_runner_prints_command() {
+  let mut input = b"".as_ref();
+  let mut output = Vec::new();
+  let mut runner = DryRunRunner;
+  let mut io = IoCtx {
+    input: &mut input,
+    output: &mut output,
+    verbose: false,
+    timing: false,
+    dry_run: true,
+  };
+  runner.run(&["git", "clone", "foo"], None, &mut io).unwrap();
+  assert_eq!(String::from_utf8(output).unwrap(), "Would run: git clone foo\n");
+}
+
+#[test]
+fn test_dry_run_runner_prints_cwd() {
+  let mut input = b"".as_ref();
+  let mut output = Vec::new();
+  let mut runner = DryRunRunner;
+  let mut io = IoCtx {
+    input: &mut input,
+    output: &mut output,
+    verbose: false,
+    timing: false,
+    dry_run: true,
+  };
+  runner.run(&["cmake", ".."], Some(Path::new("/tmp/build")), &mut io).unwrap();
+  let out = String::from_utf8(output).unwrap();
+  assert!(out.contains("Would run: cmake .."));
+  assert!(out.contains("  in directory: /tmp/build"));
 }
