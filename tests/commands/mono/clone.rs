9f0e6176c98b61be7f76eb3b3bb6afed3a501e7c
Masonlet <masonletoile@hotmail.com>
2026-06-28 17:51:17 -0400

    refactor(tests): streamline monorepo clone tests

--- tests/commands/mono/clone.rs
+++ tests/commands/mono/clone.rs
@@ -1,21 +1,31 @@
 use super::super::common::{empty_input, make_io, sink, MockRunner};
 use star_setup::commands::mono::clone::clone_mono_repos;
-use star_setup::ctx::RunCtx;
-use tempfile::TempDir;
+use star_setup::ctx::{RunCtx, Runner};
 
-#[test]
-fn test_clone_mono_repos_calls_clone_for_each_repo() {
-  let tmp = TempDir::new().unwrap();
-  let repos = vec!["user/repo1".to_string(), "user/repo2".to_string()];
+fn run_mono_test<R, F>(mut runner: R, test_logic: F) -> R
+where
+  R: Runner,
+  F: FnOnce(&std::path::Path, &mut RunCtx<'_, '_>),
+{
+  let tmp = tempfile::TempDir::new().unwrap();
   let mut input = empty_input();
   let mut output = sink();
-  let mut runner = MockRunner::new();
+
   let mut ctx = RunCtx {
     io: make_io(&mut input, &mut output),
     runner: &mut runner,
   };
 
-  clone_mono_repos(&repos, tmp.path(), false, &mut ctx).unwrap();
+  test_logic(tmp.path(), &mut ctx);
+  runner
+}
+
+#[test]
+fn test_clone_mono_repos_calls_clone_for_each_repo() {
+  let runner = run_mono_test(MockRunner::new(), |tmp_path, ctx| {
+    let repos = vec!["user/repo1".to_string(), "user/repo2".to_string()];
+    clone_mono_repos(&repos, tmp_path, false, ctx).unwrap();
+  });
 
   assert_eq!(runner.calls.len(), 2);
   assert!(runner
@@ -26,16 +36,9 @@ fn test_clone_mono_repos_calls_clone_for_each_repo() {
 
 #[test]
 fn test_clone_mono_repos_empty() {
-  let tmp = TempDir::new().unwrap();
-  let mut input = empty_input();
-  let mut output = sink();
-  let mut runner = MockRunner::new();
-  let mut ctx = RunCtx {
-    io: make_io(&mut input, &mut output),
-    runner: &mut runner,
-  };
-
-  clone_mono_repos(&[], tmp.path(), false, &mut ctx).unwrap();
+  let runner = run_mono_test(MockRunner::new(), |tmp_path, ctx| {
+    clone_mono_repos(&[], tmp_path, false, ctx).unwrap();
+  });
 
   assert!(runner.calls.is_empty());
 }
