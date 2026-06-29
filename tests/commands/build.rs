79a558cd03b5e5e05da7ca89fa6d75ceb6cc6610
Masonlet <masonletoile@hotmail.com>
2026-06-28 20:53:43 -0400

    refactor(tests): replace manual build test setup w/ with_runner_ctx

--- tests/commands/build.rs
+++ tests/commands/build.rs
@@ -1,78 +1,44 @@
-use super::common::{default_resolved_with_no_build, empty_input, make_io, sink, MockRunner};
+use super::common::{default_resolved_with_no_build, with_runner_ctx, MockRunner};
 use star_setup::{
   cli::BuildSystem,
   commands::{build_project, cmake_build, meson_build},
-  ctx::RunCtx,
 };
-use tempfile::TempDir;
 
 #[test]
 fn test_cmake_build_configure_only() {
-  let tmp = TempDir::new().unwrap();
   let args = default_resolved_with_no_build(true);
-  let mut input = empty_input();
-  let mut output = sink();
-  let mut runner = MockRunner::new();
-  let mut ctx = RunCtx {
-    io: make_io(&mut input, &mut output),
-    runner: &mut runner,
-  };
-
-  cmake_build(&args, tmp.path(), false, &mut ctx).unwrap();
-
+  let runner = with_runner_ctx(MockRunner::new(), |tmp_path, ctx| {
+    cmake_build(&args, tmp_path, false, ctx).unwrap();
+  });
   assert_eq!(runner.calls.len(), 1);
   assert!(runner.calls[0].0.contains(&"cmake".to_string()));
 }
 
 #[test]
 fn test_cmake_build_with_build_step() {
-  let tmp = TempDir::new().unwrap();
   let args = default_resolved_with_no_build(false);
-  let mut input = empty_input();
-  let mut output = sink();
-  let mut runner = MockRunner::new();
-  let mut ctx = RunCtx {
-    io: make_io(&mut input, &mut output),
-    runner: &mut runner,
-  };
-
-  cmake_build(&args, tmp.path(), false, &mut ctx).unwrap();
-
+  let runner = with_runner_ctx(MockRunner::new(), |tmp_path, ctx| {
+    cmake_build(&args, tmp_path, false, ctx).unwrap();
+  });
   assert_eq!(runner.calls.len(), 2);
   assert!(runner.calls[1].0.contains(&"--build".to_string()));
 }
 
 #[test]
 fn test_cmake_build_mono_flag() {
-  let tmp = TempDir::new().unwrap();
   let args = default_resolved_with_no_build(true);
-  let mut input = empty_input();
-  let mut output = sink();
-  let mut runner = MockRunner::new();
-  let mut ctx = RunCtx {
-    io: make_io(&mut input, &mut output),
-    runner: &mut runner,
-  };
-
-  cmake_build(&args, tmp.path(), true, &mut ctx).unwrap();
-
+  let runner = with_runner_ctx(MockRunner::new(), |tmp_path, ctx| {
+    cmake_build(&args, tmp_path, true, ctx).unwrap();
+  });
   assert!(runner.calls[0].0.contains(&"-DBUILD_LOCAL=ON".to_string()));
 }
 
 #[test]
 fn test_meson_build_configure_only() {
-  let tmp = TempDir::new().unwrap();
   let args = default_resolved_with_no_build(true);
-  let mut input = empty_input();
-  let mut output = sink();
-  let mut runner = MockRunner::new();
-  let mut ctx = RunCtx {
-    io: make_io(&mut input, &mut output),
-    runner: &mut runner,
-  };
-
-  meson_build(&args, tmp.path(), tmp.path(), &mut ctx).unwrap();
-
+  let runner = with_runner_ctx(MockRunner::new(), |tmp_path, ctx| {
+    meson_build(&args, tmp_path, tmp_path, ctx).unwrap();
+  });
   assert_eq!(runner.calls.len(), 1);
   assert!(runner.calls[0].0.contains(&"meson".to_string()));
   assert!(runner.calls[0].0.contains(&"setup".to_string()));
@@ -80,68 +46,28 @@ fn test_meson_build_configure_only() {
 
 #[test]
 fn test_meson_build_with_build_step() {
-  let tmp = TempDir::new().unwrap();
   let args = default_resolved_with_no_build(false);
-  let mut input = empty_input();
-  let mut output = sink();
-  let mut runner = MockRunner::new();
-  let mut ctx = RunCtx {
-    io: make_io(&mut input, &mut output),
-    runner: &mut runner,
-  };
-
-  meson_build(&args, tmp.path(), tmp.path(), &mut ctx).unwrap();
-
+  let runner = with_runner_ctx(MockRunner::new(), |tmp_path, ctx| {
+    meson_build(&args, tmp_path, tmp_path, ctx).unwrap();
+  });
   assert_eq!(runner.calls.len(), 2);
   assert!(runner.calls[1].0.contains(&"compile".to_string()));
 }
 
 #[test]
 fn test_build_project_dispatches_cmake() {
-  let tmp = TempDir::new().unwrap();
   let args = default_resolved_with_no_build(true);
-  let mut input = empty_input();
-  let mut output = sink();
-  let mut runner = MockRunner::new();
-  let mut ctx = RunCtx {
-    io: make_io(&mut input, &mut output),
-    runner: &mut runner,
-  };
-
-  build_project(
-    &args,
-    tmp.path(),
-    tmp.path(),
-    BuildSystem::Cmake,
-    false,
-    &mut ctx,
-  )
-  .unwrap();
-
+  let runner = with_runner_ctx(MockRunner::new(), |tmp_path, ctx| {
+    build_project(&args, tmp_path, tmp_path, BuildSystem::Cmake, false, ctx).unwrap();
+  });
   assert!(runner.calls[0].0.contains(&"cmake".to_string()));
 }
 
 #[test]
 fn test_build_project_dispatches_meson() {
-  let tmp = TempDir::new().unwrap();
   let args = default_resolved_with_no_build(true);
-  let mut input = empty_input();
-  let mut output = sink();
-  let mut runner = MockRunner::new();
-  let mut ctx = RunCtx {
-    io: make_io(&mut input, &mut output),
-    runner: &mut runner,
-  };
-
-  build_project(
-    &args,
-    tmp.path(),
-    tmp.path(),
-    BuildSystem::Meson,
-    false,
-    &mut ctx,
-  )
-  .unwrap();
-
+  let runner = with_runner_ctx(MockRunner::new(), |tmp_path, ctx| {
+    build_project(&args, tmp_path, tmp_path, BuildSystem::Meson, false, ctx).unwrap();
+  });
   assert!(runner.calls[0].0.contains(&"meson".to_string()));
 }
